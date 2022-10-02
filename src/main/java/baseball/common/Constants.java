f14f7f725f287ba4b37f5d7085e4d19a5f60626a
hganlee <msares123@gamil.com>
2022-10-03 00:45:58 +0900

    feat(RandomNumberGenerator,Constants): 랜덤 넘버 생성 기능 추가
    - 1 부터 9까지 숫자 생성 기능
    - 서로 다른 임의의 3자리 숫자 생성 기능

--- src/main/java/baseball/common/Constants.java
+++ src/main/java/baseball/common/Constants.java
@@ -2,4 +2,6 @@ package baseball.common;
 
 public class Constants {
     public static final int NUMBER_SIZE = 3;
+    public static final int RANGE_START = 1;
+    public static final int RANGE_END = 9;
 }
